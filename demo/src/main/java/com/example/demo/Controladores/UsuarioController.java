c3445798917368f2b0e4bf51580197024f87b729
gianni.beninati <gianni.beninati@alumnos.inf.utfsm.cl>
2022-01-01 13:54:10 -0300

    Estudiantes a usuario

--- demo/src/main/java/com/example/demo/Controladores/UsuarioController.java
+++ demo/src/main/java/com/example/demo/Controladores/UsuarioController.java
@@ -3,8 +3,8 @@ package com.example.demo.Controladores;
 import java.util.List;
 
 
-import com.example.demo.Entidades.Estudiantes;
-import com.example.demo.Servicios.EstudiantesServicio;
+import com.example.demo.Entidades.Usuario;
+import com.example.demo.Servicios.UsuarioServicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,10 +12,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,36 +21,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})
-@RequestMapping("/estudiantes")
-public class EstudiantesController {
+@RequestMapping("/usuario")
+public class UsuarioController {
     @Autowired
-    @Qualifier("servicioestudiantes")
-    EstudiantesServicio servicio;
+    @Qualifier("serviciousuario")
+    UsuarioServicio servicio;
 
     @GetMapping("/getAll")
-    public List<Estudiantes> getAllEstudiantes(){
+    public List<Usuario> getAllUsuario(){
         return servicio.obtenerAll();
     }
 
-    @GetMapping("/getByCorreo/{correo}")
-    public Estudiantes getEstudianteCorreo(@PathVariable("correo") String correo){
-        return servicio.encontrar(correo);
-    }
+    
 
     @PostMapping("/crear")
-    public long agregarEstudiantes(@RequestBody Estudiantes estudiante){
-        return servicio.crear(estudiante);
+    public long agregarUsuario(@RequestBody Usuario user){
+        return servicio.crear(user);
     }
 
     @DeleteMapping("/eliminar/{id}")
-    public boolean eliminarEstudiantes(@PathVariable("id") long id){
+    public boolean eliminarUsuario(@PathVariable("id") long id){
         return servicio.eliminar(id);
     }
 
-    
-    @PutMapping("/editar")
-    public boolean editarEstudiantes(@RequestBody Estudiantes estudiante){
-        return servicio.actualizar(estudiante);
-    }
     
 }
